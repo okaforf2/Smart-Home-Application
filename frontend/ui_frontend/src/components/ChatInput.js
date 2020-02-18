b0329a5d46c4ac8bd5088c6d53e67fecf9356665
Francis <okaforf2@uni.coventry.ac.uk>
2020-02-18 16:11:05 +0000

    chat component

---
+++ frontend/ui_frontend/src/components/ChatInput.js
@@ -0,0 +1,38 @@
+// NO.2
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ChatInput extends Component {
+  static propTypes = {
+    onSubmitMessage: PropTypes.func.isRequired,
+  }
+  state = {
+    message: '',
+  }
+
+  render() {
+    return (
+      <form
+        action="."
+        onSubmit={e => {
+          e.preventDefault()
+          this.props.onSubmitMessage(this.state.message)
+          this.setState({ message: '' })
+        }}
+      >
+        <label htmlFor="message">
+        Message:&nbsp;
+        <input
+          type="text"
+          placeholder={'Enter message...'}
+          value={this.state.message}
+          onChange={e => this.setState({ message: e.target.value })}
+        />
+        </label>
+        <input type="submit" value={'Send'} />
+      </form>
+    )
+  }
+}
+
+export default ChatInput
