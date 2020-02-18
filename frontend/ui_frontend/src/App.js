6402d122884d249b177b4323e9c725bd63e87d9e
Francis <okaforf2@uni.coventry.ac.uk>
2020-02-18 02:22:02 +0000

    chat component added

--- frontend/ui_frontend/src/App.js
+++ frontend/ui_frontend/src/App.js
@@ -1,45 +1,48 @@
 import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-import Input from './components/Input.js'
+import Chat from './components/Chat.js'
 
-const client = new W3CWebSocket('ws://localhost:8000');
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-        this.state = {
+    //     this.state = {
 
-        }
-    }
+    //     }
+    // }
 
-    componentWillMount() {
-        client.onopen = () => {
-            console.log('WebSocket Client Connected');
-        };
-        client.onmessage = (message) => {
-            try {
-                var jsonParsed = JSON.parse(message.data);
-                console.log(jsonParsed)
-            } catch (err) {
-                console.log(err);
-            }
-        };
-    }
+    // componentWillMount() {
+    //     client.onopen = () => {
+    //         console.log('WebSocket Client Connected');
+    //     };
+    //     client.onmessage = (message) => {
+    //         try {
+    //             var jsonParsed = JSON.parse(message.data);
+    //             console.log(jsonParsed)
+    //         } catch (err) {
+    //             console.log(err);
+    //         }
+    //     };
+    // }
 
-    handleClick(e) {
-        e.preventDefault();
-        console.log('The link was clicked.');
-        client.send("test");
-    }
+    // handleClick(e) {
+    //     e.preventDefault();
+    //     console.log('The link was clicked.');
+    //     client.send("test");
+    // }
 
     render() {
         return (
-            //<Input client={client} />
-            <a href="#" onClick={this.handleClick}>
-                Click me
-            </a>
+            <div>
+                <Chat />
+            </div>
+            // //<Input client={client} />
+            // <a href="#" onClick={this.handleClick}>
+            //     Click me
+            // </a>
         );
+         
     }
 }
 
