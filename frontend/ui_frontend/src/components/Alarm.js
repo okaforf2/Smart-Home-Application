6fd83597a16efbeea33d860c29d38f9029b878c0
jasminder <jasminder@jasminders-MacBook-Pro.local>
2020-02-19 09:04:09 +0000

    update

---
+++ frontend/ui_frontend/src/components/Alarm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../index.css'
+import 'antd/dist/antd.css';
+import { Switch, Button } from 'antd';
+
+class Alarm extends React.Component {
+    onChange(checked) {
+        console.log(`switch to ${checked}`);
+        }
+        
+        render() {
+        return (
+        <div>
+        <p>Alarm: <Switch defaultChecked onChange={this.onChange} /> </p>
+        </div>
+        );
+        } 
+        
+       }
+
+
+export default Alarm;
