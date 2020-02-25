44ae02b3b6331a4d80aec19cf0976debfb77475b
Zack Caley <zackcaleybusiness@gmail.com>
2020-02-25 03:50:18 +0000

    Stop users sending empty messages + gitignore update

--- frontend/ui_frontend/src/components/Chat.js
+++ frontend/ui_frontend/src/components/Chat.js
@@ -7,82 +7,83 @@ import ChatMessage from './ChatMessage'
 const URL = 'ws://localhost:8000'
 
 class Chat extends Component {
-  state = {
-    name: 'Bob',
-    messages: [],
-  }
+	state = {
+		name: 'Bob',
+		messages: [],
+	}
 
-  ws = new WebSocket(URL)
+	ws = new WebSocket(URL)
 
-  componentDidMount() {
-    this.ws.onopen = () => {
-      // on connecting, do nothing but log it to the console
-      console.log('connected')
-    }
+	componentDidMount() {
+		this.ws.onopen = () => {
+			// on connecting, do nothing but log it to the console
+			console.log('connected')
+		}
 
-    this.ws.onmessage = evt => {
-      // on receiving a message, add it to the list of messages
-        console.log(evt);
-        console.log(evt.name);
-        const message = JSON.parse(evt.data)
-        this.addMessage(message)
-    }
+		this.ws.onmessage = evt => {
+			// on receiving a message, add it to the list of messages
+			console.log(evt);
+			const message = JSON.parse(evt.data)
+			this.addMessage(message)
+		}
 
 
-    this.ws.onclose = () => {
-      console.log('disconnected')
-      // automatically try to reconnect on connection loss
-      this.setState({
-        ws: new WebSocket(URL),
-      })
-    }
-  }
+		this.ws.onclose = () => {
+			console.log('disconnected')
+			// automatically try to reconnect on connection loss
+			this.setState({
+				ws: new WebSocket(URL),
+			})
+		}
+	}
 
-  addMessage = message =>
-    this.setState(state => ({ messages: [message, ...state.messages] }))
+	addMessage = message =>
+		this.setState(state => ({ messages: [message, ...state.messages] }))
 
-  submitMessage = messageString => {
-    // on submitting the ChatInput form, send the message, add it to the list and reset the input
-    const message = { name: this.state.name, message: messageString }
-    this.ws.send(JSON.stringify(message))
-    this.addMessage(message)
-  }
+	submitMessage = messageString => {
+		// on submitting the ChatInput form, send the message, add it to the list and reset the input
+		if (String(messageString).length !== 0) {
+			const message = { name: this.state.name, message: messageString }
+			this.ws.send(JSON.stringify(message))
+			this.addMessage(message)
+		}
+	}
 
-  render() {
-    return (
-      <div className="NameForm">
-        <div>
-          <h1>Message Board</h1>
-        </div>
-        <label htmlFor="name">
-          Name:&nbsp;
-          <input
-            // style={{ position: 'absolute', right: '150px', width: '200px', height: '120px'}}
-            type="text"
-            id={'name'}
-            placeholder={'Enter your name...'}
-            value={this.state.name}
-            onChange={e => this.setState({ name: e.target.value })}
-          />
-        </label>
+	render() {
+		return (
+			<div className="NameForm">
+				<div>
+					<h1>Message Board</h1>
+				</div>
+				<label htmlFor="name">
+					Name:&nbsp;
+		  <input
+						// style={{ position: 'absolute', right: '150px', width: '200px', height: '120px'}}
+						type="text"
+						id={'name'}
+						placeholder={'Enter your name...'}
+						value={this.state.name}
+						onChange={e => this.setState({ name: e.target.value })}
+					/>
+				</label>
 
-        {/* Chat input component */}
-        <ChatInput
-          ws={this.ws}
-          onSubmitMessage={messageString => this.submitMessage(messageString)}
-        />
+				{/* Chat input component */}
+				<ChatInput
+					ws={this.ws}
+					onSubmitMessage={messageString => this.submitMessage(messageString)}
+				/>
 
-{/* Chat Message component */}
-        {this.state.messages.map((message, index) =>
-          <ChatMessage
-            key={index}
-            message={message.message}
-            name={message.name}
-          />,
-        )}
-      </div>
-    )
-  }
+				{/* Chat Message component */}
+				{this.state.messages.map((message, index) =>
+					<ChatMessage
+						key={index}
+						message={message.message}
+						name={message.name}
+					/>,
+				)}
+			</div>
+		)
+	}
 }
 
 export default Chat
