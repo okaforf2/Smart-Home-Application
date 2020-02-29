9a1ab4ee1bffd86efe114f387ac9a9db3d2457f1
Francis <okaforf2@uni.coventry.ac.uk>
2020-02-29 01:39:54 +0000

    Light component. True or false depending on state.

--- frontend/ui_frontend/src/App.js
+++ frontend/ui_frontend/src/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 // import { w3cwebsocket as W3CWebSocket } from "websocket";
-import Chat from './components/Chat'
-import Alarm from './components/Alarm'
+// import Chat from './components/Chat'
+// import Alarm from './components/Alarm'
 import Light from './components/Light'
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-
+const { Header, Content, Footer } = Layout;
 class App extends Component {
-  
     render() {
         return (
-            <div style={{paddingRight: 10, paddingLeft: 10}}>
-                <br />
-                <Alarm />
-                <br />
-                <Light />
-                <br />
-                <Chat />
-            </div>
-        );
+  <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Kitchen</Breadcrumb.Item>
+        <Breadcrumb.Item>Toilet</Breadcrumb.Item>
+        <Breadcrumb.Item>Garage</Breadcrumb.Item>
+      </Breadcrumb>
+      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          
+      {/* <Alarm /> */}
+      <br />
+      <Light /> 
+      <br />
+      {/* <Chat /> */}
+          
+          </div>     
+
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Team Rabbit ;)</Footer>
+  </Layout>
+
+          );
          
     }
 }
--- frontend/ui_frontend/src/components/Light.js
+++ frontend/ui_frontend/src/components/Light.js
@@ -4,16 +4,71 @@ import 'antd/dist/antd.css';
 import '../index.css';
 import { Switch } from 'antd';
 
+const URL = 'ws://localhost:8000'
+// Switch should be set to actual light status but will leave it at off for now
 class Light extends React.Component {
+    state = {
+    switchOff: false
+    };  
+      handlePressed = (e) => {
+        if (this.setState.switchOff === false ){
+          console.log('Light is off. Changing to on.');
+          this.setState.switchOff = true;
+        } else {
+          console.log('Light is on. Changing to off.');
+          this.setState.switchOff = false;
+        }
+      }
+    
 
-    onChange(checked) {
-        console.log(`switch to ${checked}`);
-    }
+	ws = new WebSocket(URL)
 
+	componentDidMount() {
+		this.ws.onopen = () => {
+			// on connecting, do nothing but log it to the console
+			console.log('connected')
+		}
+
+		this.ws.switchOff = evt => {
+			// on receiving a message, add it to the list of messages
+			console.log(evt);
+			const messageStatus = JSON.parse(evt.data)
+			this.passStatus(messageStatus)
+		}
+
+
+		// this.ws.onclose = () => {
+		// 	console.log('disconnected')
+		// 	// automatically try to reconnect on connection loss
+		// 	this.setState({
+		// 		ws: new WebSocket(URL),
+		// 	})
+		// }
+	}
+
+	passStatus = messageStatus =>
+		this.setState(state => ({ switchOff: [messageStatus, ...state.switchOff] }))
+
+	// submitMessage = messageString => {
+	// 	// on submitting the ChatInput form, send the message, add it to the list and reset the input
+	// 	if (String(messageString).length !== 0) {
+	// 		const message = { type: 'message', name: this.state.name, message: messageString }
+	// 		this.ws.send(JSON.stringify(message))
+	// 		this.addMessage(message)
+	// 	}
+	// }
+
+
+    // onChange(checked) {
+    //     console.log(`switch to ${checked}`);
+    // }
+
+    
     render() {
+        // const buttonPressed = this.state.buttonPressed
         return (
             <div>
-                <p>Light: <Switch defaultChecked onChange={this.onChange} /> </p>
+                <p>Light: <Switch defaultChecked   onClick={this.handlePressed} /> </p>
             </div>
         );
     }
